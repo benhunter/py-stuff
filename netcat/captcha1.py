d0e939e2ebf4ebdfee6429edaf1c01b02ed4f765
Ben <code@benhunter.me>
2018-01-30 23:40:43 +0900

    working on https://netcat.us/challenges/intermediate/intermediate4/
    Signed-off-by: Ben <code@benhunter.me>

--- netcat/captcha1.py
+++ netcat/captcha1.py
@@ -15,30 +15,31 @@ regex = re.compile(b'Incorrect')
 incorrect = 0
 rounds = 50
 
-for i in range(rounds):
-    command = 'php -r \'$rnd = 1; srand(time()); $rnd &= rand(); echo $rnd;\''  # or add 1 second to time?
-    cmd_output = subprocess.check_output(command, shell=True)
-    guess = cmd_output.decode()
-    # print('guess:', guess)
-    r = requests.post('http://netcat.us:6655/', data={'guess': guess})
-    # print(r.cookies)
-    # print(r.content)
-
-    if regex.search(r.content):
-        print(r.content)
-        incorrect += 1
-
-    print(i)
-
-print("Incorrect: ", incorrect)
-print("% Correct: ", str((rounds - incorrect) / rounds * 100) + '%')
+# creates a new session every time
+# for i in range(rounds):
+#     command = 'php -r \'$rnd = 1; srand(time()); $rnd &= rand(); echo $rnd;\''  # or add 1 second to time?
+#     cmd_output = subprocess.check_output(command, shell=True)
+#     guess = cmd_output.decode()
+#     # print('guess:', guess)
+#     r = requests.post('http://netcat.us:6655/', data={'guess': guess})
+#     # print(r.cookies)
+#     # print(r.content)
+#
+#     if regex.search(r.content):
+#         print(r.content)
+#         incorrect += 1
+#
+#     print(i)
+#
+# print("Incorrect: ", incorrect)
+# print("% Correct: ", str((rounds - incorrect) / rounds * 100) + '%')
 
 #
 # r = s.get('http://netcat.us:6655/')
 # r = s.post('http://netcat.us:6655/', data={'guess': 0})
 
 #
-# round = 3
+
 
 '''  PHP Code
 $time = 1517226214;
@@ -49,22 +50,23 @@ srand($time);
 $rnd &= rand();
 '''
 
-# for i in range(20):
-#     command = 'php -r \'echo $rnd = mt_rand(1, ' + str(round) + '); srand(time()+1); $rnd &= rand();\''
-#     cmd_output = subprocess.check_output(command, shell=True)
-#     guess = cmd_output.decode()
-#     print('guess:', guess)
-#     # guess &= now
-#     r = s.post('http://netcat.us:6655/', data={'guess': guess})
-#     # print(s.cookies)
-#     # print(r.content)
-#     round += 1
-#     print('round: ', round)
-#     print('cookie round: ', s.cookies)
-#     print()
-#
-# print(s.cookies)
-# print(r.content)
+round = 3
+for i in range(20):
+    command = 'php -r \'echo $rnd = mt_rand(1, ' + str(round) + '); srand(time()+1); $rnd &= rand();\''
+    cmd_output = subprocess.check_output(command, shell=True)
+    guess = cmd_output.decode()
+    print('guess:', guess)
+    # guess &= now
+    r = s.post('http://netcat.us:6655/', data={'guess': guess})
+    # print(s.cookies)
+    # print(r.content)
+    round += 1
+    print('round: ', round)
+    print('cookie round: ', s.cookies)
+    print()
+
+print(s.cookies)
+print(r.content)
 
 
 # cookies = r.cookies
