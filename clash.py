45aa2a31b0066153ef44b9d08b748f58204d86fb
ben <code@benhunter.me>
2018-09-25 11:27:55 +0900

    Design Patterns. Removed useless file.

--- clash.py
+++
@@ -1,6 +0,0 @@
-n = int('321')
-last = str(n)[-1]
-
-print(n)
-print(last)
-print(str(n)[-2:])
---
+++ designpatterns/singleton.py
@@ -0,0 +1,39 @@
+# https://web.archive.org/web/20060612061259/http://www.suttoncourtenay.org.uk/duncan/accu/pythonpatterns.html
+
+'''
+Singleton pattern. Use of singleton is driven by shared state. See also Borg pattern. Also note that modules are effectively singletons.
+'''
+
+
+class Singleton(object):
+    '''
+    Only one instance of the class can be instantiated.
+    '''
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(Singleton, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+
+
+class ChildOfSingleton(Singleton):
+    pass
+
+
+c = ChildOfSingleton()
+d = ChildOfSingleton()
+
+id(c)
+id(d)
+
+
+class Borg:
+    '''
+    All Borg objects share the same symbol table of writable attributes, __dict__
+    '''
+    __shared_state = {}
+
+    def __init__(self):
+        self.__dict__ = self.__shared_state
+    # and whatever else is needed in the class
