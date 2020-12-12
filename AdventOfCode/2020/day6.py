3e66ba2a79819be0bdb9ae7c874482d9fc7498c9
Ben Hunter <code@benhunter.me>
2020-12-11 17:41:52 -0700

    Advent of Code Day 6

---
+++ AdventOfCode/2020/day6.py
@@ -0,0 +1,36 @@
+# Advent of Code Day 6
+# https://adventofcode.com/2020/day/6
+
+from collections import OrderedDict
+import string
+
+with open("day6-input.txt") as f:
+    lines = f.read()  # read file to a single string
+
+# make a list of groups. Two newlines seperate records.
+groups = lines.split("\n\n")
+
+part1 = 0
+part2 = 0
+
+for i, g in enumerate(groups):
+
+    # Part 1
+    groups[i] = "".join(sorted(set("".join(g.split()))))
+    part1 += len(groups[i])
+
+    # Part 2
+    people = g.split()
+    if len(people) == 1:
+        part2 += len(people[0])
+    elif len(people) > 1:
+        group = "".join(sorted("".join(g.split())))
+        
+        for letter in string.ascii_lowercase:
+            if group.count(letter) == len(people):
+                part2 += 1
+            
+
+
+print("Part 1:", part1)
+print("Part 2:", part2)
