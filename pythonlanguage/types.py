03197fd10fdebe28ef1d322ccf6e7ab573e818ba
Ben Hunter <code@benhunter.me>
2021-04-20 07:19:19 -0700

    List insertions

--- pythonlanguage/types.py
+++ pythonlanguage/types.py
@@ -3,6 +3,11 @@
 first_dict = {0: False, 1: False}
 second_dict = dict.fromkeys(first_dict, True)
 
+# max of dict
+third_dict = {1:34, 3:8}
+md = max(third_dict)
+print(md)  # prints 3 - the max key
+ 
 
 # Slicing
 string = "abcdefghijklmnopqrstuvwxyz"
@@ -12,4 +17,25 @@ string = "abcdefghijklmnopqrstuvwxyz"
 
 for index in range(len(string) - 1):
     print(string[:index + 1], string[index + 1:])
-    
+
+
+
+# Insert a list into another list - efficiently?
+inner = list(range(3))
+outer = list(range(10))
+destination_index = 2
+# place inner into outer after destination_index
+# option 1 fastest?
+outer[destination_index + 1 : destination_index + 1] = inner
+# option 2
+outer = outer[:destination_index + 1] + inner + outer[destination_index + 1:]
+# option 3
+outer = [*outer[:destination_index + 1], *inner, *outer[destination_index + 1:]]
+# option 4
+new_outer = outer[:destination_index + 1]
+new_outer += inner
+new_outer += outer[destination_index + 1:]
+outer = new_outer
+# fails
+outer.insert(destination_index + 1, inner)  # does not unpack inner
+outer.insert(destination_index + 1, *inner)  # TODO test unpacking
